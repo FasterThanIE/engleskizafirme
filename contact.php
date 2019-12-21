a6f9a2df46cd98e3908584940baba6e6f09a8e2f
Localhost user <dijanic@gmail.com>
2019-12-21 21:52:07 +0100

    * Added missing structure

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+
+
+<html>
+
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+
+    <body>
+
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+
+</html>
