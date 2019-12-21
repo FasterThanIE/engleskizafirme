e121c1a46d44b097d84934c6e53901ea15dcad6f
Localhost user <dijanic@gmail.com>
2019-12-21 22:00:29 +0100

    * Napravljena navigacija na sajtu

--- contact.php
+++ contact.php
@@ -6,9 +6,12 @@
     <head>
 
         <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/navigacija.css" />
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
 
     <body>
+
+        <?php require_once "delovi_stranica/index/navigacija.php"; ?>
 
         <script src="js/jquery-3.4.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
---
+++ delovi_stranica/index/navigacija.php
@@ -0,0 +1,5 @@
+<nav class="jumbotron">
+
+    <p class="testKlasa">Hello world</p>
+
+</nav>
