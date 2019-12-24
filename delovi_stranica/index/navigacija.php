3125b12358075bf00080967652762002f0cfbb49
komunalac <cirjakovicmilos8@gmail.com>
2019-12-24 15:26:10 +0100

    navigacija prepravljena

---
+++ delovi_stranica/index/navigacija.php
@@ -0,0 +1,35 @@
+
+
+<div class="fixed-top">
+            <img src="img/navigacija.png" alt="">
+            <nav class="navbar navbar-expand-lg col-lg-12 fixed-top">
+                <a class="navbar-brand" href="index.php">
+                    <img src="img/brod.png" class="logo ml-5" alt="">
+                </a>
+
+                <button class="navbar-toggler border border-light" type="button" data-toggle="collapse" data-target="#collapseNav">
+
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+
+                <div class="collapse navbar-collapse" id="collapseNav">
+
+                    <ul class="navbar-nav ml-auto">
+
+                        <li class="nav-item mr-3">
+                            <a class="nav-link text-white" href="index.php">O NAMA</a>
+                        </li>
+
+                        <li class="nav-item mr-3">
+                            <a class="nav-link text-white" href="">UTISCI POLAZNIKA</a>
+                        </li>
+
+                        <li class="nav-item mr-5">
+                            <a class="nav-link text-white" href="">KONTAKT</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
