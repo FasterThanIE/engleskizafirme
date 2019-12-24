8b8022aef83e3ecc8e3cd83e4a02092c3187585a
mirjam-jegic <mirjam-jegic@users.noreply.github.com>
2019-12-24 19:58:36 +0100

    Merge branch 'master' of C:\xampp\htdocs\engleskizafirme with conflicts.

---
+++ delovi_stranica/index/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+<body>
+
+
+
+
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,50 +4,11 @@
 <html>
 
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link rel="stylesheet" type="text/css" href="css/style.css" />
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
-
+        <link rel="stylesheet" type="text/css" href="css/header.css" />
     <body>
-
-        <div class="fixed-top">
-            <img src="img/navigacija.png" alt="">
-            <nav class="navbar navbar-expand-lg col-lg-12 fixed-top">
-                <a class="navbar-brand" href="index.php">
-                    <img src="img/brod.png" class="logo ml-5" alt="">
-                </a>
-
-                <button class="navbar-toggler border border-light" type="button" data-toggle="collapse" data-target="#collapseNav">
-
-                    <span class="navbar-toggler-icon"></span>
-
-                </button>
-
-                <div class="collapse navbar-collapse" id="collapseNav">
-
-                    <ul class="navbar-nav ml-auto">
-
-                        <li class="nav-item mr-3">
-                            <a class="nav-link text-white" href="index.php">O NAMA</a>
-                        </li>
-
-                        <li class="nav-item mr-3">
-                            <a class="nav-link text-white" href="">UTISCI POLAZNIKA</a>
-                        </li>
-
-                        <li class="nav-item mr-5">
-                            <a class="nav-link text-white" href="">KONTAKT</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-        </div>
-
-
-
-
 
         <script src="js/jquery-3.4.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
