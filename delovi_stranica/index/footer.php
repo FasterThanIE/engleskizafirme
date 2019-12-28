b88f748dc5388cc2c5e1852cca74b6928bc6997a
komunalac <cirjakovicmilos8@gmail.com>
2019-12-28 02:33:49 +0100

    footer uradjen

---
+++ delovi_stranica/index/footer.php
@@ -0,0 +1,52 @@
+
+    <div class="row fontFooter footerBackground p-5">
+
+        <div class="col-xl-6 col-lg-12">
+
+            <div class="row col-xl-5 col-lg-12 mx-auto">
+                <div class="card my-4 text-center w-100">
+                    <div class="card-body">
+                        <p class="card-text">KONTAKT TELEFON</p>
+                    </div>
+                    <div class="card-body footerCardBlue text-white">
+                        <p class="card-text">064 161 07 00</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row  col-xl-5 col-lg-12 mx-auto">
+                <div class="card my-4 text-center w-100">
+                    <div class="card-body">
+                        <p class="card-text">EMAIL</p>
+                    </div>
+                    <div class="card-body footerCardRed text-white">
+                        <p class="card-text">COLICIVO@YAHOO.COM</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-6 col-lg-12 background">
+            <h1 class="pl-2"><span class="footerH1">KONTAKTIRAJTE</span> NAS</h1>
+            <form action="" method="POST">
+
+                <div class="form-group col-12">
+                    <input type="text" class="form-control form-control-lg rounded-0" name="" id="" placeholder="Ime i prezime.">
+                </div>
+
+                <div class="form-group col-12">
+                    <input type="text" class="form-control form-control-lg rounded-0" name="" id="" placeholder="Kontakt email.">
+                </div>
+
+                <div class="form-group col-12">
+                    <textarea class="form-control form-control-lg rounded-0" id="" name="" rows="6" placeholder="Text poruke"></textarea>
+                </div>
+
+
+                <button type="submit" class="btn btn-lg btnForm text-white float-right mr-3 rounded-0 px-4" id="">POŠALJI PORUKU</button>
+
+            </form>
+        </div>
+    </div>
+<
+
--- index.php
+++ index.php
@@ -5,18 +5,21 @@
 
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/style.css" />
         <link rel="stylesheet" type="text/css" href="css/navigacija.css" />
         <link rel="stylesheet" type="text/css" href="css/predznanje.css" />
         <link rel="stylesheet" type="text/css" href="css/iskustva.css" />
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+        <link rel="stylesheet" type="text/css" href="css/footer.css" />
+
 
     <body>
 
         <?php require_once "delovi_stranica/index/navigacija.php"; ?>
         <?php require_once "delovi_stranica/index/predznanje.php"; ?>
         <?php require_once "delovi_stranica/index/iskustva.php"; ?>
+        <?php require_once "delovi_stranica/index/footer.php"; ?>
 
         <script src="js/jquery-3.4.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
