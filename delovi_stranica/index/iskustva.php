3f8514432645d77cdcc78093d178a93b7728df28
komunalac <cirjakovicmilos8@gmail.com>
2019-12-28 04:11:05 +0100

    footer uradjen

--- delovi_stranica/index/iskustva.php
+++ delovi_stranica/index/iskustva.php
@@ -1,56 +1,139 @@
+<div class="d-xs-block d-md-none d-xl-block  iskustvaBackground iskustvaFont text-center">
+    <h1 class="mb-0 pt-5"><span class="iskustvaBlue">ISKUSTVA</span> POLAZNIKA</h1>
 
-<h1 class="text-center iskustvaBackground mb-0 pt-5 iskustvaFont"><span class="iskustvaBlue">ISKUSTVA</span> POLAZNIKA</h1>
+    <div class="col-12 d-flex justify-content-center">
 
-<div class="col-12 d-flex justify-content-center iskustvaBackground">
+        <div class="card-deck col-9 py-5">
 
-    <div class="card-deck iskustvaFont col-9 py-5">
-
-        <div class="card">
-            <div class="d-flex justify-content-center">
-                <div class="iskustvaSlika pt-5">
-                    <img src="img/iskustva_slika.png" class="card-img-top">
+            <div class="card">
+                <div class="d-flex justify-content-center">
+                    <div class="iskustvaSlika pt-5">
+                        <img src="img/iskustva_slika.png" class="card-img-top">
+                    </div>
                 </div>
-            </div>
-            <div class="card-body text-center">
-                <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
-                <h6 class="iskustvaText">ITSKOLA.NET</h6>
-                <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                <div class="card-body">
+                    <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                    <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                    <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
                     Lorem Ipsum has been the industry's standard dummy
                     text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
                     to make a type specimen book.</p>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="d-flex justify-content-center">
-                <div class="iskustvaSlika pt-5">
-                    <img src="img/iskustva_slika.png" class="card-img-top">
                 </div>
             </div>
-            <div class="card-body text-center">
-                <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
-                <h6 class="iskustvaText">ITSKOLA.NET</h6>
-                <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+
+            <div class="card">
+                <div class="d-flex justify-content-center">
+                    <div class="iskustvaSlika pt-5">
+                        <img src="img/iskustva_slika.png" class="card-img-top">
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                    <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                    <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
                     Lorem Ipsum has been the industry's standard dummy
                     text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
                     to make a type specimen book.</p>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="d-flex justify-content-center">
-                <div class="iskustvaSlika pt-5">
-                    <img src="img/iskustva_slika.png" class="card-img-top">
                 </div>
             </div>
-            <div class="card-body text-center">
-                <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
-                <h6 class="iskustvaText">ITSKOLA.NET</h6>
-                <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+
+            <div class="card">
+                <div class="d-flex justify-content-center">
+                    <div class="iskustvaSlika pt-5">
+                        <img src="img/iskustva_slika.png" class="card-img-top">
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                    <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                    <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
                     Lorem Ipsum has been the industry's standard dummy
                     text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
                     to make a type specimen book.</p>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+    <div class="d-none d-md-block d-xl-none iskustvaBackground iskustvaFont pb-5">
+        <h1 class="mb-0 pt-5 text-center"><span class="iskustvaBlue">ISKUSTVA</span> POLAZNIKA</h1>
+        <div class="card m-5">
+
+            <div class="row no-gutters">
+
+                <div class="col-4 d-flex justify-content-center">
+
+                    <div class="iskustvaSlika pt-md-5 pt-lg-4">
+                        <img src="img/iskustva_slika.png" class="card-img" alt="...">
+                    </div>
+
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                        <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                        <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy
+                            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                            to make a type specimen book</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card m-5">
+
+            <div class="row no-gutters">
+
+                <div class="col-4 d-flex justify-content-center">
+
+                    <div class="iskustvaSlika pt-md-5 pt-lg-4">
+                        <img src="img/iskustva_slika.png" class="card-img" alt="...">
+                    </div>
+
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                        <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                        <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy
+                            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                            to make a type specimen book</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mt-5 mx-5">
+
+            <div class="row no-gutters">
+
+                <div class="col-4 d-flex justify-content-center">
+
+                    <div class="iskustvaSlika pt-md-5 pt-lg-4">
+                        <img src="img/iskustva_slika.png" class="card-img" alt="...">
+                    </div>
+
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h4 class="card-title iskustvaIme">Tomislav Nikolic</h4>
+                        <h6 class="iskustvaText">ITSKOLA.NET</h6>
+                        <p class="card-text iskustvaText">Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy
+                            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                            to make a type specimen book</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
