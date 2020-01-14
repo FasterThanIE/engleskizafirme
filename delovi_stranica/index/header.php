be96fe6367f0c6b0a679bb5a5c5957ff635de9b9
mirjam-jegic <mirjam-jegic@users.noreply.github.com>
2020-01-14 23:38:13 +0100

    Prepravljen header

--- delovi_stranica/index/header.php
+++ delovi_stranica/index/header.php
@@ -1,13 +1,22 @@
 
 <header class="d-flex justify-content-center" id="about-us">
-    <div class="text-field col-lg-6 col-md-6 col-sm-8 col-10 justify-content-center">
-       <h1 class="naslov p-3 border-black">KURSEVI ENGLESKOG</h1>
-        <div class="opis p-2 border-black">The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)
-            that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless
-            filler text can be ...</div>
-       <div class="d-flex justify-content-center">
-        <button class="btn-lg col-8 mt-lg-4 contact-btn">KONTAKTIRAJTE NAS</button>
-      </div>
+    <div class="text-field p-5 justify-content-center">
+
+        <div class="opis border-black ">
+
+            <h1 class="naslov border-black justify-content-center align-items-center d-flex">KURSEVI ENGLESKOG</h1>
+            <p>The purpose of lorem ipsum is to create a natural<br>
+            looking block of text (sentence, paragraph, page, etc.)<br>
+            that doesn't distract from the layout. A practice not<br>
+            without controversy, laying out pages with meaningless<br>
+                filler text can be ...</p>
+
+             <div class="d-flex justify-content-center">
+                  <button class="btn contact-btn">KONTAKTIRAJTE NAS</button>
+             </div>
+
+        </div>
+
     </div>
 </header>
 
