ad74dfe268b89749c6573d9596de0706e4fb295b
tomicawarr@gmail.com <dijanic@gmail.com>
2020-01-17 19:56:12 +0100

    * Fontsize default

--- delovi_stranica/index/predznanje.php
+++ delovi_stranica/index/predznanje.php
@@ -2,7 +2,7 @@
     <div class="col-xl-11 col-xs-12 pt-5">
         <h1><span class="colorBlue">PREDZNANJE</span> <span class="colorRed">NIJE POTREBNO</span></h1>
 
-        <p class="colorGray pb-5">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+        <p class="colorGray pb-5" style="font-size: 1.1em;">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
             voluptatum deleniti
             atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
             sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
